03561606566d6a76603aa6d88edff52cea72b986
Kanuri suresh <96762472+sureshArnold@users.noreply.github.com>
2026-07-24 17:20:10 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloud-terraform-basics"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "Terraform-state-locking"
-    encrypt        = true
-  }
-}
